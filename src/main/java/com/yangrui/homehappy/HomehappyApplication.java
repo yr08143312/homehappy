ded5d2b8f17b708d933e28f5d332cfd907df505f
yangrui <yr08143312@163.com>
2020-07-22 22:44:41 +0800

    Initial commit

---
+++ src/main/java/com/yangrui/homehappy/HomehappyApplication.java
@@ -0,0 +1,13 @@
+package com.yangrui.homehappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomehappyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomehappyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangrui/homehappy/HomehappyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangrui.homehappy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomehappyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
